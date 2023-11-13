4d727c19a80e004e46756dc4b725650b21f0d2ec
Nikko Pham <nikkopham@Nikkos-MacBook-Air.local>
2023-11-14 05:44:38 +1300

    Commit 22

---
+++ 23.11.13_homework07_ModuleExcercise/main.py
@@ -0,0 +1,17 @@
+from dto import InventoryItem, ItemOrigin 
+
+def main():
+    item_origin = ItemOrigin(country= "Ethiopia", production_date = "02/12/2023") #create object
+    my_item1 = InventoryItem(name = "printer", 
+                             quantity = 5, 
+                             serial_number = "HDOUHKJN",
+                             origin = item_origin)
+    my_serialised_object1 = my_item1.__dict__
+    print (my_serialised_object1)
+    my_item2 = InventoryItem(**my_serialised_object1) #** break the object down into properties and passes them to InventoryItem
+    print(my_item2.__dict__)
+
+
+
+if __name__ == "__main__":
+    main()
