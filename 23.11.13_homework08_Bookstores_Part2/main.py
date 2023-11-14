3e44f9c0d4e9ffa9108354d5e4474a8f9ee2161d
Nikko Pham <nikkopham@Nikkos-MacBook-Air.local>
2023-11-15 08:06:45 +1300

    Commit 25

---
+++ 23.11.13_homework08_Bookstores_Part2/main.py
@@ -0,0 +1,22 @@
+from classes import Author, BookItem, BookStore
+from pydantic import ValidationError 
+
+def main():
+    try:
+        author_info1 = Author(name = "John Doe", author_id = "JODO-1234")
+        author_info2 = Author(name = "Alana Grey", author_id = "ALGR-1234")
+
+        book_item1 = BookItem(name="The Sky is Blue", author=author_info1, year_published = 2024)
+        book_item2 = BookItem(name="What to eat today?", author=author_info2, year_published = 2020)
+    
+        book_store = BookStore(bookstore_name="Whitcoulls", book_shelf=[book_item1, book_item2])
+    
+    except ValidationError as ve: 
+        print("The Author's information/Book information has to be valid.")
+    else: 
+        bookstore = book_store.__dict__
+        print(bookstore)
+    
+if __name__=="__main__":
+    main()
+
