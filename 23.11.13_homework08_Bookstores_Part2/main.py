ec674521235d9251c5b98adfe177a13cbba6c291
Nikko Pham <nikkopham@Nikkos-MacBook-Air.local>
2023-11-15 18:36:04 +1300

    Commit 26

--- 23.11.13_homework08_Bookstores_Part2/main.py
+++ 23.11.13_homework08_Bookstores_Part2/main.py
@@ -10,12 +10,16 @@ def main():
         book_item2 = BookItem(name="What to eat today?", author=author_info2, year_published = 2020)
     
         book_store = BookStore(bookstore_name="Whitcoulls", book_shelf=[book_item1, book_item2])
-    
+              
     except ValidationError as ve: 
         print("The Author's information/Book information has to be valid.")
+    except: #Catch other error that I may not consider
+        print("Another Error")
     else: 
         bookstore = book_store.__dict__
         print(bookstore)
+    #finally: #Whatever errors happen, the code will still print the final.
+        #print("Finally")
     
 if __name__=="__main__":
     main()
