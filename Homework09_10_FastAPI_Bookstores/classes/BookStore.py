c28d1f9a211f387714bf33cd963a6b7bdf7f3b72
Nikko Pham <nikkopham@Nikkos-MacBook-Air.local>
2023-12-08 18:31:17 +1300

    Commit 32

--- Homework09_10_FastAPI_Bookstores/classes/BookStore.py
+++
@@ -1,6 +0,0 @@
-from pydantic import BaseModel, field_validator
-from .BookItem import BookItem
-
-class BookStore(BaseModel):
-    bookstore_name: str
-    book_shelf: list[BookItem]
--- Homework09_10_FastAPI_Bookstores/classes/__init__.py
+++
@@ -1,5 +0,0 @@
-from pydantic import BaseModel, field_validator
-from .Author import Author
-from .BookItem import BookItem
-from .BookStore import BookStore
-
